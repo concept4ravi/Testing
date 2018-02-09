9ae4a46cec7ceac0311258e141fe9a87aa28d34a
intern <intern@localhost>
2018-02-09 11:05:48 +0530

    Commit

--- AmazonTesting/src/SignInTest.java
+++ AmazonTesting/src/SignInTest.java
@@ -1,18 +1,7 @@
 
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -20,14 +9,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class SignInTest extends RequiredData{
-	
+
 	Boolean flag;
-	private String sheetName = "Sheet1";
 	private WebDriver driver;
 	@BeforeTest
 	@Parameters({"browser"})
@@ -42,29 +29,37 @@ public class SignInTest extends RequiredData{
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
 
-	@Test(dataProvider = "Login")
-	public void signIn(String uname,String password) throws InterruptedException {
+	@Test
+	public void launchSite() {
 		driver.get(baseUrl);
 		try {
 			flag=driver.findElement(By.id("nav-logo")).isDisplayed();
-		
+
 		}catch(Exception e) {
 			flag=false;
 			System.out.println(e.getMessage());
 		}
 		Assert.assertTrue(flag, "Unable to access "+baseUrl);
+	}
+
+	@Test
+	public void openSignInPage() {
 		try {
 			flag=driver.findElement(By.cssSelector("#nav-link-yourAccount > span.nav-line-1")).isDisplayed();
-		
+
 		}catch(Exception e) {
 			flag=false;
 			System.out.println(e.getMessage());
 		}
 		Assert.assertTrue(flag, "Sign In Option Not available");
+	}
+
+	@Test(dataProvider = "Login")
+	public void login(String uname,String password) {
 		driver.findElement(By.cssSelector("#nav-link-yourAccount > span.nav-line-1")).click();
 		try {
 			flag=driver.findElement(By.id("ap_email")).isDisplayed();
-		
+
 		}catch(Exception e) {
 			flag=false;
 			System.out.println(e.getMessage());
@@ -73,11 +68,10 @@ public class SignInTest extends RequiredData{
 		driver.findElement(By.id("ap_email")).click();
 		driver.findElement(By.id("ap_email")).clear();
 		driver.findElement(By.id("ap_email")).sendKeys(uname);
-
 		driver.findElement(By.cssSelector("span.a-button-inner > #continue")).click();
 		try {
 			flag=driver.findElement(By.id("ap_password")).isDisplayed();
-		
+
 		}catch(Exception e) {
 			flag=false;
 			System.out.println(e.getMessage());
@@ -87,12 +81,20 @@ public class SignInTest extends RequiredData{
 		driver.findElement(By.id("ap_password")).clear();
 		driver.findElement(By.id("ap_password")).sendKeys(password);
 		driver.findElement(By.id("signInSubmit")).click();
+	}
+
+	@Test
+	public void validateLogin() throws InterruptedException {
 		Thread.sleep(1000);
 		WebElement link = driver.findElement(By.id("nav-link-yourAccount"));
 		boolean b = link.findElement(By.cssSelector(".nav-line-1")).getText().contains("Hello");
-		Assert.assertTrue(b,"Login Failed for Username "+uname+" with Password '"+password+"'");
-		System.out.println("Username "+uname+" with Password '"+password+"' is Successfully Logged In");
+		Assert.assertTrue(b,"Login Failed Failed");
+	}
+
+	@Test
+	public void deleteCookies() {
 		driver.manage().deleteAllCookies();
 	}
+
 
 }
